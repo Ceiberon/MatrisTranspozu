409e354e24629c80fec3fdaf2d29841cf8ebaa8c
Ceiberon <ethem_641@hotmail.com>
2024-03-15 01:31:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+        };
+
+
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
